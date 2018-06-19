30c18586cd113f10ad981b52f392f1d78e8ca294
Mẫn <vanman.lqd@gmail.com>
2018-06-19 11:21:32 +0700

    added login layout

--- ClubManagement/ClubManagement/MainActivity.cs
+++ ClubManagement/ClubManagement/MainActivity.cs
@@ -12,7 +12,7 @@ namespace ClubManagement
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.activity_login);
         }
     }
 }
--- ClubManagement/ClubManagement/Resources/Resource.Designer.cs
+++ ClubManagement/ClubManagement/Resources/Resource.Designer.cs
@@ -31,6 +31,39 @@ namespace ClubManagement
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010000
+			public const int cardBackgroundColor = 2130771968;
+			
+			// aapt resource value: 0x7f010001
+			public const int cardCornerRadius = 2130771969;
+			
+			// aapt resource value: 0x7f010002
+			public const int cardElevation = 2130771970;
+			
+			// aapt resource value: 0x7f010003
+			public const int cardMaxElevation = 2130771971;
+			
+			// aapt resource value: 0x7f010005
+			public const int cardPreventCornerOverlap = 2130771973;
+			
+			// aapt resource value: 0x7f010004
+			public const int cardUseCompatPadding = 2130771972;
+			
+			// aapt resource value: 0x7f010006
+			public const int contentPadding = 2130771974;
+			
+			// aapt resource value: 0x7f01000a
+			public const int contentPaddingBottom = 2130771978;
+			
+			// aapt resource value: 0x7f010007
+			public const int contentPaddingLeft = 2130771975;
+			
+			// aapt resource value: 0x7f010008
+			public const int contentPaddingRight = 2130771976;
+			
+			// aapt resource value: 0x7f010009
+			public const int contentPaddingTop = 2130771977;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -41,11 +74,183 @@ namespace ClubManagement
 			}
 		}
 		
-		public partial class Layout
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f05000c
+			public const int background_color = 2131034124;
+			
+			// aapt resource value: 0x7f050009
+			public const int button_background_color_focus = 2131034121;
+			
+			// aapt resource value: 0x7f050008
+			public const int button_background_color_unfocus = 2131034120;
+			
+			// aapt resource value: 0x7f05000b
+			public const int cardview_background_color = 2131034123;
+			
+			// aapt resource value: 0x7f050000
+			public const int cardview_dark_background = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int cardview_light_background = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int cardview_shadow_end_color = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int cardview_shadow_start_color = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int colorAccent = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int colorPrimary = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int colorPrimaryDark = 2131034117;
+			
+			// aapt resource value: 0x7f05000a
+			public const int edit_text_border_color_focus = 2131034122;
+			
+			// aapt resource value: 0x7f050007
+			public const int edit_text_border_color_unfocus = 2131034119;
+			
+			// aapt resource value: 0x7f05000e
+			public const int text_color_black = 2131034126;
+			
+			// aapt resource value: 0x7f05000d
+			public const int text_color_green = 2131034125;
+			
+			// aapt resource value: 0x7f05000f
+			public const int text_color_white = 2131034127;
+			
+			// aapt resource value: 0x7f050010
+			public const int text_hint_color = 2131034128;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f06000c
+			public const int button_margin_bottom = 2131099660;
+			
+			// aapt resource value: 0x7f060000
+			public const int cardview_compat_inset_shadow = 2131099648;
+			
+			// aapt resource value: 0x7f060006
+			public const int cardview_conner_radius = 2131099654;
+			
+			// aapt resource value: 0x7f060001
+			public const int cardview_default_elevation = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int cardview_default_radius = 2131099650;
+			
+			// aapt resource value: 0x7f060005
+			public const int cardview_margin = 2131099653;
+			
+			// aapt resource value: 0x7f060007
+			public const int cardview_padding_left_right = 2131099655;
+			
+			// aapt resource value: 0x7f060008
+			public const int carview_padding_top_bottom = 2131099656;
+			
+			// aapt resource value: 0x7f060003
+			public const int edit_text_border_stroke_width = 2131099651;
+			
+			// aapt resource value: 0x7f06000b
+			public const int edit_text_margin_bottom = 2131099659;
+			
+			// aapt resource value: 0x7f060009
+			public const int edit_text_padding_left_right = 2131099657;
+			
+			// aapt resource value: 0x7f06000a
+			public const int edit_text_padding_top_bottom = 2131099658;
+			
+			// aapt resource value: 0x7f06000d
+			public const int text_welcome_size = 2131099661;
+			
+			// aapt resource value: 0x7f060004
+			public const int view_conner_radius = 2131099652;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
+			{
+			}
+		}
+		
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int button_background = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int button_background_focus = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int button_background_unfocus = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int edit_text_border_focus = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int edit_text_border_unfocus = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int edittext_states = 2130837509;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f080000
+			public const int cardView1 = 2131230720;
+			
+			// aapt resource value: 0x7f080001
+			public const int linearLayout1 = 2131230721;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int activity_login = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
@@ -60,8 +265,26 @@ namespace ClubManagement
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f070000
+			public const int app_name = 2131165184;
+			
+			// aapt resource value: 0x7f070003
+			public const int button_sign_in = 2131165187;
+			
+			// aapt resource value: 0x7f070004
+			public const int button_sign_up = 2131165188;
+			
+			// aapt resource value: 0x7f070001
+			public const int email_hint = 2131165185;
+			
+			// aapt resource value: 0x7f070002
+			public const int password_hint = 2131165186;
+			
+			// aapt resource value: 0x7f070005
+			public const int text_view_had_account = 2131165189;
+			
+			// aapt resource value: 0x7f070006
+			public const int textview_welcome = 2131165190;
 			
 			static String()
 			{
@@ -69,6 +292,98 @@ namespace ClubManagement
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f040001
+			public const int Base_CardView = 2130968577;
+			
+			// aapt resource value: 0x7f040000
+			public const int CardView = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int CardView_Dark = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int CardView_Light = 2130968579;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] CardView = new int[] {
+					16843071,
+					16843072,
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972,
+					2130771973,
+					2130771974,
+					2130771975,
+					2130771976,
+					2130771977,
+					2130771978};
+			
+			// aapt resource value: 1
+			public const int CardView_android_minHeight = 1;
+			
+			// aapt resource value: 0
+			public const int CardView_android_minWidth = 0;
+			
+			// aapt resource value: 2
+			public const int CardView_cardBackgroundColor = 2;
+			
+			// aapt resource value: 3
+			public const int CardView_cardCornerRadius = 3;
+			
+			// aapt resource value: 4
+			public const int CardView_cardElevation = 4;
+			
+			// aapt resource value: 5
+			public const int CardView_cardMaxElevation = 5;
+			
+			// aapt resource value: 7
+			public const int CardView_cardPreventCornerOverlap = 7;
+			
+			// aapt resource value: 6
+			public const int CardView_cardUseCompatPadding = 6;
+			
+			// aapt resource value: 8
+			public const int CardView_contentPadding = 8;
+			
+			// aapt resource value: 12
+			public const int CardView_contentPaddingBottom = 12;
+			
+			// aapt resource value: 9
+			public const int CardView_contentPaddingLeft = 9;
+			
+			// aapt resource value: 10
+			public const int CardView_contentPaddingRight = 10;
+			
+			// aapt resource value: 11
+			public const int CardView_contentPaddingTop = 11;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
