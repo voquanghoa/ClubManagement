265dd91142e0d7ab6cd7a75ff9ca71b7c4d7e300
Mẫn <vanman.lqd@gmail.com>
2018-07-02 11:07:09 +0700

    Alert dialog to progress dialog

--- ClubManagement/ClubManagement/Ultilities/DialogExtensions.cs
+++ ClubManagement/ClubManagement/Ultilities/DialogExtensions.cs
@@ -5,13 +5,18 @@ namespace ClubManagement.Ultilities
 {
     public static class DialogExtensions
     {
-        public static AlertDialog CreateDialog(string title, string message, Context context)
+#pragma warning disable 618
+        public static ProgressDialog CreateDialog(string title, string message, Context context)
+#pragma warning restore 618
         {
-            return new AlertDialog.Builder(context)
-                .SetCancelable(false)
-                .SetTitle(title)
-                .SetMessage(message)
-                .Create();
+#pragma warning disable 618
+            var progressDialog = new ProgressDialog(context);
+#pragma warning restore 618
+            progressDialog.SetTitle(title);
+            progressDialog.SetMessage(message);
+            progressDialog.SetCancelable(false);
+            progressDialog.SetProgressStyle(ProgressDialogStyle.Spinner);
+            return progressDialog;
         }
     }
 }
