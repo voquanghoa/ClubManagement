de30c9d7e2e7c668b62f4af4856804193c2df5fe
Nguyễn Văn Mẫn <man_gareth@outlook.com.vn>
2018-08-28 17:15:32 +0700

    resize image before upload

--- ClubManagement/ClubManagement/Controllers/CloudinaryController.cs
+++ ClubManagement/ClubManagement/Controllers/CloudinaryController.cs
@@ -1,6 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using Android.Content;
+using Android.Graphics;
+using Android.Net;
+using Android.Provider;
 using ClubManagement.Ultilities;
+using Java.IO;
 
 namespace ClubManagement.Controllers
 {
@@ -12,7 +17,7 @@ namespace ClubManagement.Controllers
 
         private const string ApiSecret = "ventEbxZBzh3g5EAfVw7htDIbDA";
 
-        public static async Task<string> UploadImage(Context context, Android.Net.Uri imageUri, string publicId)
+        public static async Task<string> UploadImage(Context context, Uri imageUri, string publicId)
         {
             var account =
                 new CloudinaryDotNet.Account(CloudName, ApiKey, ApiSecret);
@@ -21,13 +26,31 @@ namespace ClubManagement.Controllers
 
             var uploadParams = new CloudinaryDotNet.Actions.ImageUploadParams
             {
-                File = new CloudinaryDotNet.Actions.FileDescription(FilePathUtilities.GetAbsoluteFilePath(context, imageUri)),
+                File = new CloudinaryDotNet.Actions.FileDescription(FilePathUtilities.GetAbsoluteFilePath(context, GetImageUri(context, ResizeImage(context, imageUri, 256)))),
                 PublicId = publicId
             };
 
             var uploadResult = await cloudinary.UploadAsync(uploadParams);
 
             return uploadResult.Uri.OriginalString;
+        }
+
+        private static Bitmap ResizeImage(Context context, Uri uri, int requireSize)
+        {
+            var bitmap = MediaStore.Images.Media.GetBitmap(context.ContentResolver, uri);
+            var inWidth = bitmap.Width;
+            var inHeight = bitmap.Height;
+
+            return inWidth > inHeight
+                ? Bitmap.CreateScaledBitmap(bitmap, requireSize, inHeight * requireSize / inWidth, false)
+                : Bitmap.CreateScaledBitmap(bitmap, inWidth * requireSize / inHeight, requireSize, false);
+        }
+
+        private static Uri GetImageUri(Context inContext, Bitmap inImage)
+        {
+            inImage.Compress(Bitmap.CompressFormat.Jpeg, 100, new MemoryStream());
+            var path = MediaStore.Images.Media.InsertImage(inContext.ContentResolver, inImage, "Title", null);
+            return Uri.Parse(path);
         }
     }
 }
