cf923197e4636b0c5b39fbad1ac99b945747cd3d
Huynh Ngoc Minh <huynhngocminh2511@gmail.com>
2018-09-07 13:14:27 +0700

    Add info marker layout

--- ClubManagement/ClubManagement/Activities/Base/MapAbstractActivity.cs
+++ ClubManagement/ClubManagement/Activities/Base/MapAbstractActivity.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
 using Android.Views;
-using Android.Widget;
 using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
 using ClubManagement.Ultilities;
 using static Android.Gms.Maps.GoogleMap;
+using Android.Widget;
 using Android.Graphics;
 
 namespace ClubManagement.Activities.Base
@@ -55,30 +49,12 @@ namespace ClubManagement.Activities.Base
 
         public View GetInfoWindow(Marker marker)
         {
-            LinearLayout info = new LinearLayout(this)
-            {
-                Orientation = Orientation.Vertical
-            };
+            var view = LayoutInflater.Inflate(Resource.Layout.InfoMarker, null);
 
-            info.SetBackgroundColor(Color.Argb(200, 255, 255, 255));
-            info.SetPadding(4, 4, 4, 4);
+            view.FindViewById<TextView>(Resource.Id.tvTitle).Text = marker.Title;
+            view.FindViewById<TextView>(Resource.Id.tvSnippet).Text = marker.Snippet;
 
-            var title = new TextView(this);
-            title.SetTextColor(Color.Black);
-            title.Gravity = GravityFlags.Center;
-            title.SetTypeface(null, TypefaceStyle.Bold);
-            title.SetText(marker.Title, TextView.BufferType.Normal);
-
-            var snippet = new TextView(this);
-            title.SetTextColor(Color.Black);
-            title.Gravity = GravityFlags.Center;
-            title.SetTypeface(null, TypefaceStyle.Bold);
-            snippet.SetText(marker.Snippet, TextView.BufferType.Normal);
-
-            info.AddView(title);
-            info.AddView(snippet);
-
-            return info;
+            return view;
         }
     }
 }
