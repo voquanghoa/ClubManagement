c96cf774fc5969b28a91710a246942624ebafdb0
Huynh Ngoc Minh <huynhngocminh2511@gmail.com>
2018-09-18 15:32:14 +0700

    redefine date and time on event item

--- ClubManagement/ClubManagement/Ultilities/DateTimeExtensions.cs
+++ ClubManagement/ClubManagement/Ultilities/DateTimeExtensions.cs
@@ -12,6 +12,22 @@ namespace ClubManagement.Ultilities
             return start.AddDays(daysToAdd);
         }
 
+        public static string ToDateTimeString(this DateTime startTime, DateTime endTime)
+        {
+            return startTime.Day == endTime.Day
+                ? startTime.ToString($"MMM dd \u00B7 h{startTime.ToTimeString()} tt") +
+                    " - " +
+                    endTime.ToString($"h{endTime.ToTimeString()} tt")
+                : startTime.ToString("MMM dd") + " - " + endTime.ToString("MMM dd");
+        }
+
+        private static string ToTimeString(this DateTime time)
+        {
+            return time.Minute == 0
+                ? ""
+                : time.ToString(":mm");
+        }
+
         public static bool IsInThisWeek(this DateTime dateTime)
         {
             var today = DateTime.Today;
