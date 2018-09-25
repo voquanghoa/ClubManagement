38c3b7c32a7ece38292e3ad89cb9193260e20c8f
Nguyễn Văn Mẫn <man_gareth@outlook.com.vn>
2018-09-25 19:09:36 +0700

    refactor income tab

---
+++ ClubManagement/ClubManagement/CustomAdapters/IncomeViewHolder.cs
@@ -0,0 +1,41 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using ClubManagement.Models;
+using ClubManagement.Ultilities;
+
+namespace ClubManagement.CustomAdapters
+{
+    public class IncomeViewHolder : RecyclerView.ViewHolder
+    {
+        [InjectView(Resource.Id.imgGroup)] private ImageView imgGroup;
+
+        [InjectView(Resource.Id.tvTitle)] private TextView tvTitle;
+
+        [InjectView(Resource.Id.tvAmount)] private TextView tvAmount;
+
+        [InjectView(Resource.Id.tvTime)] private TextView tvTime;
+
+        [InjectView(Resource.Id.pbAmount)] private ProgressBar pbAmount;
+
+        public IncomeModel IncomeModel
+        {
+            set
+            {
+                tvTitle.Text = value.Description;
+                tvTime.Text = value.Time.ToString("MMM dd, yyyy");
+                var totalAmount = value.NumberOfUsers * value.Amount;
+                var paidAmount = value.NumberOfPaidUsers * value.Amount;
+                var progress = (int) (paidAmount * 100 / totalAmount);
+                tvAmount.Text = $"{paidAmount:N0}/{totalAmount.ToCurrency()}";
+                pbAmount.Progress = progress;
+                pbAmount.ProgressTintList = ItemView.Context.Resources.GetColorStateList(paidAmount < totalAmount ? Resource.Color.color_red : Resource.Color.color_blue, null);
+            }
+        }
+
+        public IncomeViewHolder(View itemView) : base(itemView)
+        {
+            Cheeseknife.Inject(this, itemView);
+        }
+    }
+}
---
+++ ClubManagement/ClubManagement/Fragments/BalanceListFragment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Support.V4.App;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using ClubManagement.CustomAdapters;
+using ClubManagement.Models;
+
+namespace ClubManagement.Fragments
+{
+    public class BalanceListFragment : Fragment
+    {
+        [InjectView(Resource.Id.rvBalance)] private RecyclerView rvBalance;
+
+        private IncomeListAdapter incomeListAdapter;
+
+        public BalanceListFragment()
+        {
+            incomeListAdapter = new IncomeListAdapter
+            {
+                IncomeModels = new List<IncomeModel>()
+            };
+        }
+
+        public List<IncomeModel> IncomeModels
+        {
+            set
+            {
+                if (incomeListAdapter == null)
+                {
+                    incomeListAdapter = new IncomeListAdapter();
+                }
+
+                incomeListAdapter.IncomeModels = value;
+            }
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.fragment_balance_recyclerview, container, false);
+            Cheeseknife.Inject(this, view);
+            Init();
+            return view;
+        }
+
+        private void Init()
+        {
+            rvBalance.SetLayoutManager(new LinearLayoutManager(Context));
+            rvBalance.SetAdapter(incomeListAdapter);
+        }
+    }
+}
