4b929adab1ae7bc7283639e18eb9482fd08b6d84
Nguyễn Văn Mẫn <man_gareth@outlook.com.vn>
2018-10-03 22:04:50 +0700

    wip

--- ClubManagement/ClubManagement/Activities/AddAmountActivity.cs
+++ ClubManagement/ClubManagement/Activities/AddAmountActivity.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Widget;
+using ClubManagement.CustomAdapters;
+using ClubManagement.Models;
 using ClubManagement.Ultilities;
 
 namespace ClubManagement.Activities
@@ -8,16 +13,39 @@ namespace ClubManagement.Activities
     [Activity(Label = "AddAmountActivity")]
     public class AddAmountActivity : Activity
     {
+        private AmountItemListAdapter adapter;
+
+        [InjectView(Resource.Id.tvTotal)] private TextView tvTotal;
+
+        [InjectView(Resource.Id.rvAmountItems)]
+        private RecyclerView rvItems;
+
+        [InjectOnClick(Resource.Id.tvAddItem)]
+        private void AddItem(object s, EventArgs e)
+        {
+            this.ShowMessage("asbdkasdksakjd");
+            adapter.AddItem(new AmountItem
+            {
+                Item = new OutcomeAmountItem
+                {
+                    Name = "2",
+                    Amount = 123123
+                },
+                IsChooseToDelete = false
+            });
+        }
+
         [InjectOnClick(Resource.Id.btnBack)]
         private void Back(object s, EventArgs e)
         {
-            Finish();
+            //Finish();
+            adapter.DeleteChoosedItem();
         }
 
         [InjectOnClick(Resource.Id.btnDelete)]
         private void Delete(object s, EventArgs e)
         {
-            this.ShowMessage("sadasdas");
+            adapter.IsDeleting = true;
         }
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -25,6 +53,61 @@ namespace ClubManagement.Activities
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_add_amount);
             Cheeseknife.Inject(this);
+            Init();
+        }
+
+        private void Init()
+        {
+            adapter = new AmountItemListAdapter
+            {
+                Items = new List<AmountItem>
+                {
+                    new AmountItem
+                    {
+                        Item = new OutcomeAmountItem
+                        {
+                            Name = "1",
+                            Amount = 1000
+                        }
+                    },
+                    new AmountItem
+                    {
+                        Item = new OutcomeAmountItem
+                        {
+                            Name = "2",
+                            Amount = 1000
+                        }
+                    },
+                    new AmountItem
+                    {
+                        Item = new OutcomeAmountItem
+                        {
+                            Name = "3",
+                            Amount = 1000
+                        }
+                    },
+                    new AmountItem
+                    {
+                        Item = new OutcomeAmountItem
+                        {
+                            Name = "4",
+                            Amount = 1000
+                        },
+                        IsChooseToDelete = false
+                    },
+                    new AmountItem
+                    {
+                        Item = new OutcomeAmountItem
+                        {
+                            Name = "5",
+                            Amount = 1000
+                        }
+                    }
+                }
+            };
+
+            rvItems.SetLayoutManager(new LinearLayoutManager(this));
+            rvItems.SetAdapter(adapter);
         }
     }
 }
---
+++ ClubManagement/ClubManagement/CustomAdapters/AmountItemListAdapter.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Android.Support.V7.Widget;
+using Android.Views;
+using ClubManagement.Models;
+
+namespace ClubManagement.CustomAdapters
+{
+    public class AmountItemListAdapter : RecyclerView.Adapter
+    {
+        public List<AmountItem> Items { get; set; }
+
+        private bool isDeleting = false;
+
+        public bool IsDeleting
+        {
+            get => isDeleting;
+
+            set
+            {
+                isDeleting = value;
+                Items.ForEach(x => x.IsDeleting = value);
+                NotifyDataSetChanged();
+            }
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            if (holder is AmountItemViewHolder viewHolder)
+            {
+                viewHolder.Item = Items[position];
+                viewHolder.IsDeleting = IsDeleting;
+                viewHolder.Click += (s, e) =>
+                {
+                    if (s is bool isChoose)
+                    {
+                        Items[position].IsChooseToDelete = isChoose;
+                        viewHolder.Item = Items[position];
+                    }
+                };
+            }
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.item_amount, parent, false);
+            return new AmountItemViewHolder(view);
+        }
+
+        public override int ItemCount => Items.Count;
+
+        public void AddItem(AmountItem item)
+        {
+            Items.Add(item);
+            NotifyDataSetChanged();
+        }
+
+        public void DeleteChoosedItem()
+        {
+            foreach (var item in Items.ToList())
+            {
+                if (item.IsChooseToDelete)
+                {
+                    Items.Remove(item);
+                }
+            }
+            NotifyDataSetChanged();
+        }
+    }
+}
